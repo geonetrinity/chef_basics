21d8728b505c1c49d8ce515c19b5706606497405
geonetrinity@gmail.com <geonetrinity@gmail.com>
2022-02-18 08:00:46 +0800

    update user repo

--- cookbooks/create_user_group/recipes/default.rb
+++ cookbooks/create_user_group/recipes/default.rb
@@ -3,3 +3,26 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+
+#--Create a Group
+group 'cloudops' do
+  comment 'Creating group named cloudops'
+  action :create
+end
+
+#--Create a user
+user 'george' do
+  comment 'create user george'
+  home '/home/george'
+  shell '/bin/bash'
+  password 'password123'
+  action  :create
+end
+
+#--Add user to group
+group 'cloudops' do
+  comment 'add user to group'
+  action :modify
+  members 'george'
+  append true
+end
